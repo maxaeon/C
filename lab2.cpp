a5aa25d39118b21d313d0a24525c1b04e4201ef5
M.A. Parks (they/them) <maxaeonparks@gmail.com>
2022-09-25 17:51:26 -0400

    Create lab2.cpp

---
+++ lab2.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+/* complete Lab 2 program that allows the user to
+   enter a time in seconds and then outputs how far
+   an object would drop if it is in free fall for
+   that length of time. Assume no friction or resistance
+   from air and a constant acceleration of 32 feet per second
+   due to gravity. Use the equation
+      Distance = 1/2 * acceleration * (time * time)
+    all output values must be labeled and formatted to three places to
+    the right of the decimal
+    test your code using 10, 20 and 30 seconds*/
+int main()
+{   // variables list Distance, acceleration , time
+    cout<<setiosflags(ios::fixed|ios::showpoint);
+    // prompt user for time in seconds
+    // calculate distance
+    // output results
+    cout << "\n\n Lab 2" << endl;
+    return 0;
+}
